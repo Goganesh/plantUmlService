29df00ef5f75ad88876e294e0a5245763f3a0b69
Басиладзе Георгий Тамазович <gbasiladze@phoenixit.ru>
2021-10-28 16:07:12 +0300

    fixik

--- src/main/java/com/goganesh/packages/service/FileWriterService.java
+++ src/main/java/com/goganesh/packages/service/FileWriterService.java
@@ -1,10 +1,7 @@
 package com.goganesh.packages.service;
 
-import org.springframework.stereotype.Service;
-
 import java.io.*;
 
-@Service
 public interface FileWriterService {
 
     void writeFile(String from, String to) throws IOException;
